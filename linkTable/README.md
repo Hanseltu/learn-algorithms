5d73b9a895c1a1ee257ba53691de3dd6945f506c
Hanseltu <tuhaoxin6@gmail.com>
2018-04-07 15:53:21 +0800

    master

--- linkTable/README.md
+++ linkTable/README.md
@@ -21,3 +21,15 @@ node->next = list->next;
 list->next = node;
 ```
 
+* **尾插法**建立单链表(关键步骤)
+```c
+node->data = &data;
+while(p->next)  //将指针移到最后一个结点
+{
+    p = p->next;
+}
+node->next = NULL;
+p-next = node;
+```
+
+
--- linkTable/s-link.c
+++ linkTable/s-link.c
@@ -20,18 +20,45 @@ LinkListPtr creat_head(int n){
 
     list = (LinkListPtr)malloc(sizeof(Node));
     list->next = NULL;
-    //printf("Please input the number of the array \n");
+    //printf("Please input the number of the single linktable: \n");
     for (i = 0; i < n; i++)
     {
         int a[] = {1,2,3,4,5,6,7,8,9};
         node = (LinkListPtr)malloc(sizeof(Node));
         //scanf("%d",&data);
-        node->data = a[i];
+        node->data = a[i];   /* 关键步骤 */
         node->next = list->next;
         list->next = node;
     }
     return list;
 }
+
+/* 尾插法建立单链表 */
+LinkListPtr creat_tail(int n){
+    LinkListPtr list,node,p;
+    //ElemType data;
+    int i;
+
+    list = (LinkListPtr)malloc(sizeof(Node));
+    list->next = NULL;
+    p = list;
+    //printf("Please input the number of the single linktable: \n");
+    for (i = 0; i < n;i++)
+    {
+        node = (LinkListPtr)malloc(sizeof(Node));
+        //scanf("%d",&data);
+        int a[] = {1,2,3,4,5,6,7,8,9,10};
+        node->data = a[i];  /* 关键步骤 */
+        while(p->next) /* 指针移到最后一个结点 */
+        {
+            p = p->next;
+        }
+        node->next = NULL;
+        p->next = node;
+    }
+    return list;
+}
+
 
 Status printLinkList(LinkListPtr list){
     LinkListPtr p;
@@ -45,8 +72,8 @@ Status printLinkList(LinkListPtr list){
     return 0;
 }
 int main(){
-
-    LinkListPtr list = creat_head(10);
+    LinkListPtr list = creat_tail(10);
+    //LinkListPtr list = creat_head(10);
     printLinkList(list);
     return 0;
 }
