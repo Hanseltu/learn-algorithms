cdc020e344c44d9d91309e3ffa14a1cd4fc13f76
Hanseltu <tuhaoxin6@gmail.com>
2018-04-11 14:35:09 +0800

    master

--- stack&queue/stack.c
+++ stack&queue/stack.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#include<malloc.h>
-
+#include<bool.h>
+/*
 #define STACK_INIT_SIZE 10
 #define STACKINCREMENT 2
 #define TRUE 1
@@ -21,21 +21,20 @@ typedef struct SqStack{
     int stacksize;
 }SqStack;
 
-/* 顺序栈的9个基本操作 */
 
-/* 1.构建一个空栈 */
+
 Status InitStack(SqStack* S){
     S->base = (int*)malloc(sizeof(ElemType)*STACK_INIT_SIZE);
     if (!S->base)
     {
-        exit(OVERFLOW);  /* 存储分配失败 */
+        exit(OVERFLOW);
     }
     S->top = S->base;
     S->stacksize = STACK_INIT_SIZE;
     return OK;
 }
 
-/* 2.销毁栈 */
+
 Status DestroyStack(SqStack *S){
     S->top = NULL;
     S->stacksize = 0;
@@ -43,13 +42,12 @@ Status DestroyStack(SqStack *S){
     return OK;
 }
 
-/* 3.清空栈 */
 Status ClearStack(SqStack *S){
     S->top = S->base;
     return OK;
 }
 
-/* 4.判断栈是否为空，非空返回1 */
+
 Status StackEmpty(SqStack *S){
     if (S->top == S->base)
     {
@@ -61,7 +59,7 @@ Status StackEmpty(SqStack *S){
     }
 }
 
-/* 5.求栈的长度*/
+
 Status StackLength(SqStack *S){
     if(S->top == S->base)
     {
@@ -73,7 +71,6 @@ Status StackLength(SqStack *S){
     }
 }
 
-/* 6.求栈顶元素 */
 Status GetTop(SqStack *S, ElemType e){
     if (S->top == S->base)
     {
@@ -86,7 +83,6 @@ Status GetTop(SqStack *S, ElemType e){
     return e;
 }
 
-/* 7.栈顶插入元素 */
 Status Push(SqStack *S,ElemType e){
     if (S->top - S->base >= STACK_INIT_SIZE)
     {
@@ -104,7 +100,7 @@ Status Push(SqStack *S,ElemType e){
 }
 
 
-/* 8.删除栈顶元素 */
+
 Status Pop(SqStack *S,ElemType e){
     if (S->top == S->base)
     {
@@ -118,7 +114,6 @@ Status Pop(SqStack *S,ElemType e){
     }
 }
 
-/* 9.遍历栈 */
 Status StackTraverse(SqStack *S){
     if (S->base == NULL)
     {
@@ -150,4 +145,118 @@ int main(){
     printf ("----------------");
     StackTraverse(S);
     return OK;
+}
+
+*/
+
+#define STACK_SIZE 20
+typedef struct stack{
+    int top;
+    int array[STACK_SIZE];
+}STACK;
+
+STACK S;
+
+// init
+int initStack(){
+    S.top = 0;
+    return 0;
+}
+
+// isEmpty
+bool isEmpty(){
+    if(0 == S.top)
+        return true;
+    else
+        return false;
+}
+
+// isFull
+bool isFull(){
+    if(STACK_SIZE == S.top)
+        return true;
+    else
+        return false;
+}
+
+// push
+int push(int e){
+    if(isFull()){
+        printf("stack overflow! stop push element\n");
+        return -1;
+    }else{
+        S.top++;
+        S.array[S.top-1] = e;
+        return S.top;
+    }
+}
+
+// pop
+int pop(){
+    if (isEmpty()) {
+        printf("stack empty! pop null\n");
+    }else{
+        S.top--;
+        return S.array[S.top+1];
+    }
+}
+
+// clear
+void clear(){
+    while(!isEmpty()){
+        pop();
+    }
+}
+
+// printAll
+void printAll(){
+    if(isEmpty())
+        printf("empty stack! print nothing");
+    int i = 0;
+    for(i=0; i< S.top; i++){
+        printf("%d  ", S.array[i]);
+    }
+    printf("\n");
+}
+
+// getLength
+int getLength(){
+    return S.top;
+}
+
+
+int main(void){
+
+    initStack();
+
+    int i = 0;
+    for (i = 0; i < 10; i++) {
+        int ret = push(i);
+        if(-1 == ret)
+            break;
+    }
+
+    printAll();
+
+    pop();
+
+    printAll();
+
+    for (i = 11; i < 30; i++) {
+        int ret = push(i);
+        if(-1 == ret)
+            break;
+    }
+
+    printAll();
+
+    printf("getLength: %d\n", getLength());
+
+    printf("clear all\n");
+    clear();
+
+    printf("getLength: %d\n", getLength());
+    printAll();
+
+    return 0;
 }
