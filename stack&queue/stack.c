a250c025d49563e01a09a047a94c6ac9ca65db68
Hanseltu <tuhaoxin6@gmail.com>
2018-04-13 15:17:44 +0800

    master

--- stack&queue/stack.c
+++ stack&queue/stack.c
@@ -10,33 +10,42 @@ struct Stack{
 };
 typedef struct Stack *slink;
 
+//建立栈
 slink create_stack(int capability){
 	slink stack=malloc(sizeof(struct Stack));
 	if(!stack)
 		return stack;
 	stack->capability=capability;
 	stack->sp=-1;
-	stack->data=malloc(sizeof(int)*capability);
+	stack->data=(int*)malloc(sizeof(int)*capability);
 	return stack;
 }
 
+
+//判断栈空
 int empty(slink stack){
 	return stack->sp==-1;
 }
 
+//判断栈满
 int full(slink stack){
 	return stack->sp==stack->capability-1;
 }
 
+//入栈操作
 void push_stack(slink stack,int item){
 	stack->data[++stack->sp]=item;
 }
 
+//出栈操作
 int pop_stack(slink stack){
 	if(empty(stack))
 		return -1;
 	return stack->data[stack->sp--];
 }
+
+
+
 
 int main(){
 	slink stack=create_stack(5);
